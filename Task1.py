1125cf27177693419077626aaf611d4df5b2f443
vijayasharma11 <vijayatidke@gmail.com>
2024-12-06 10:40:34 +0000

    create new Program add, multiply and dividing

---
+++ Task1.py
@@ -0,0 +1,11 @@
+# Get two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the two numbers
+total = num1 + num2
+total = num1 * num2
+total = num1 / num2
+
+# Display the result
+print("The total of", num1, "and", num2, "is", total)
